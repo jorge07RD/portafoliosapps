51afeea2c9a74f6215c22de9416e6b506f4a75af
jorge <jorje.elduro3@gmail.com>
2021-08-03 11:11:10 -0400

    pruebass

--- portafolios/settings.py
+++ portafolios/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bemfz^^iy#dmnt#@@o5qlxqil3k2-qtj*212#+8=_$*dcr^e4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
