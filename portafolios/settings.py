0fb77c54e8e0b1d92a605a9c78c946a3c471b81b
jorge <jorje.elduro3@gmail.com>
2021-08-03 16:10:22 -0400

    enviar email 1.0

--- portafolios/settings.py
+++ portafolios/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bemfz^^iy#dmnt#@@o5qlxqil3k2-qtj*212#+8=_$*dcr^e4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -144,5 +144,3 @@ EMAIL_HOST_PASSWORD="070797123"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
