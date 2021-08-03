d5db17cf22c4a1af63ae70ca3f1a870539f994f1
jorge <jorje.elduro3@gmail.com>
2021-08-02 22:26:38 -0400

    dalefafa

--- portafolios/settings.py
+++ portafolios/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "portafolioapp",
     'bulma',
     "django_static_fontawesome",
-    "portafolioapp"
+    
 ]
 
 MIDDLEWARE = [
